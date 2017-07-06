f91f5e10dad4faae81b7c512f9b4a9e1d31c0aaa
Bruno Macedo <bhfmacedo@gmail.com>
2017-07-06 12:31:12 -0300

    Further adjusts

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const KEY = 'ntalk.sid', SECRET = 'ntalk';
 var express = require('express')
   , load = require('express-load')
   , bodyParser = require('body-parser')
@@ -8,26 +9,45 @@ var express = require('express')
   , app = express()
   , server = require('http').Server(app)
   , io = require('socket.io')(server)
+  , cookie = cookieParser(SECRET)
+  , store = new expressSession.MemoryStore()
 ;
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(cookieParser('ntalk'));
-app.use(expressSession());
+app.use(cookie);
+app.use(expressSession({
+  secret: SECRET,
+  name: KEY,
+  resave: true,
+  saveUninitializaed: true,
+  store: store
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 app.use(express.static(__dirname + '/public'));
+
+io.use(function(socket, next) {
+  var data = socket.request;
+  cookie(data, {}, function(err){
+    var sessionID = data.signedCookies[KEY];
+    store.get(sessionID, function(err, session) {
+      if (err || !session) {
+        return next(new Error('acesso negado'));
+      } else {
+        socket.handshake.session = session;
+        return next();
+      }
+    })
+  })
+})
+
 load('models')
   .then('controllers')
   .then('routes')
   .into(app);
-io.sockets.on('connection', function(client) {
-  client.on('send-server', function(data) {
-    var msg = "<b>"+data.nome+":<b>"+data.msg+"<br>";
-    client.emit('send-client', msg);
-    client.broadcast.emit('send-client', msg);
-  }); 
-});
+load('sockets')
+  .into(io);
 
 app.use(error.notFound);
 app.use(error.serverError);
